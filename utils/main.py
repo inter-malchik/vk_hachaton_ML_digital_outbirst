50a042cd8c07dd60bdb861f158fc200c939d49e3
Anton Subbota <66995955+Anton2813@users.noreply.github.com>
2022-08-27 06:37:00 +0300

    finished UserParser or utils
    finished UserParser or utils

---
+++ utils/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+from .utils import *
+
+df = pd.read_csv("lies.csv", delimiter='\t')
+
+# clear df
+count = 0
+for index, row in enumerate(df.itertuples()):
+    if not is_correct_row(row):
+        df.drop(index)
+        count += 1
+
+
+print(count)
